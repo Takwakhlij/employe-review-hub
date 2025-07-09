1c81d35999fe6400eaec2953b1e8142a8fe2c835
Takwakhlij <takwakhlij2004@gmail.com>
2025-07-09 13:58:55 +0100

    employe-review

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+app.use(helmet());
+
+// Connexion MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log(" MongoDB Connected"))
+  .catch(err => console.error(" MongoDB Error", err));
+
+// Test route
+app.get('/', (req, res) => res.send('API Running'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
